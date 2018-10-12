64c74d838c229bdaec71fc54c9cd6524e79005d0
mingzi <mingzicao0407@163.com>
2018-10-11 21:12:28 -0400

    classic Binary Search

---
+++ src/main/java/ClassicBinarySearch.java
@@ -0,0 +1,49 @@
+/*
+
+Given a target integer T and an integer array A sorted in ascending order,
+    find the index i such that A[i] == T
+    or return -1 if there is no such index.
+
+    Assumptions
+    There can be duplicate elements in the array,
+    and you can return any of the indices i such that A[i] == T.
+
+ */
+
+public class ClassicBinarySearch {
+
+    public int binarySearch(int[] array, int target) {
+        if (array == null || array.length == 0) {
+            return -1;
+        }
+        int left = 0;
+        int right = array.length -1;
+        while (left < right - 1) {
+            int mid = left + (right - left)/2;
+            if (array[mid] == target) {
+                return mid;
+            } else if (array[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        if (array[left] == target) {
+            return left;
+        }
+
+        if (array[right] == target) {
+            return right;
+        }
+        return -1;
+    }
+
+}
+
+
+/*Possible Bugs :
+  1. infinite loop:
+     why
+     if there are two elements [1, 3], target = 8
+     mid +1, mid -1, requires at least 3 elements to ensure the mid index is valid
+*/
---
+++ src/test/java/ClassicBinarySearchTest.java
@@ -0,0 +1,91 @@
+import org.junit.jupiter.api.Test;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.anyOf;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ClassicBinarySearchTest {
+
+    public ClassicBinarySearch cbs = new ClassicBinarySearch();;
+
+    private int NOT_FOUND = -1;
+
+    private int[] input;
+
+
+    @Test
+    public void testNull () {
+        int result = cbs.binarySearch(input, 0);
+        assertEquals( NOT_FOUND, result);
+    }
+
+    @Test
+    public void testEmptyArray() {
+        input = new int[]{};
+        int result = cbs.binarySearch(input, 0);
+        assertEquals( NOT_FOUND, result);
+    }
+
+    @Test
+    public void testOneElementArrayNotFound() {
+        input = new int[]{1};
+        int result = cbs.binarySearch(input, 0);
+        assertEquals( NOT_FOUND, result);
+
+    }
+
+    @Test
+    public void testOneElementArray() {
+        input = new int[]{1};
+        int result = cbs.binarySearch(input, 1);
+        assertEquals(0, result);
+
+    }
+
+    @Test
+    public void testTwoElementArrayLeft() {
+        input = new int[] {1,3};
+        int result = cbs.binarySearch(input, 1);
+        assertEquals(0, result);
+    }
+
+    @Test
+    public void testTwoElementArrayRight() {
+        input = new int[] {1,3};
+        int result = cbs.binarySearch(input, 3);
+        assertEquals(1, result);
+    }
+
+    @Test  //Fail Case
+    public void testTwoElementArrayNotFould2() {
+        input = new int[] {1,2};
+        int result = cbs.binarySearch(input, 0);
+        assertEquals(NOT_FOUND, result);
+    }
+    @Test
+    public void testTwoElementArrayNotFound() {
+        input = new int[] {1,3};
+        int result = cbs.binarySearch(input, 2);
+        assertEquals(NOT_FOUND, result);
+    }
+
+    @Test
+    public void testLongArray() {
+       input  = new int [] {1, 2, 2, 2, 3, 4};
+       int result = cbs.binarySearch(input, 2);
+       assertThat(result, anyOf(equalTo(1), equalTo(2), equalTo(3)));
+
+    }
+
+    @Test
+    public void testLongArrayNotfound() {
+        input  = new int [] {1, 2, 2, 2, 3, 4};
+        int result = cbs.binarySearch(input, 5);
+        assertEquals(NOT_FOUND, result);
+
+    }
+
+
+
+}
